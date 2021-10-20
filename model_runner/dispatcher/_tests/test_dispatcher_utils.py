7fc198834bd39fe08f543ab2c875fa3b5ccba110
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2021-10-20 11:28:53 +0200

    add dispatcher

---
+++ model_runner/dispatcher/_tests/test_dispatcher_utils.py
@@ -0,0 +1,31 @@
+import pytest
+
+from model_runner.dispatcher.dispatcher_utils import create_run_command
+
+
+def test_create_run_command():
+    params = {
+        "runner": "test.py",
+        "data": "path_to_data",
+        "batch_size": 1,
+        "learning_rate": 0.01,
+    }
+    run_command = create_run_command(params)
+
+    expected_command = "test.py --data path_to_data --batch_size 1 --learning_rate 0.01"
+    assert run_command == expected_command
+
+
+def test_create_run_command_errors():
+    params_no_runner = {"data": "path_to_data", "batch_size": 1, "learning_rate": 0.01}
+    with pytest.raises(KeyError):
+        _ = create_run_command(params_no_runner)
+
+    params_bad_runner = {
+        "runner": 5,
+        "data": "path_to_data",
+        "batch_size": 1,
+        "learning_rate": 0.01,
+    }
+    with pytest.raises(TypeError):
+        _ = create_run_command(params_bad_runner)
---
+++ model_runner/dispatcher/dispatcher.py
@@ -0,0 +1,21 @@
+import argparse
+import json
+import subprocess
+
+from .dispatcher_utils import create_run_command
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("job_id", help="the job ID from /$LSB_JOBINDEX", type=int)
+    parser.add_argument("params", help="path to the job params file", type=str)
+    args = parser.parse_args()
+
+    job_id = args.job_id
+
+    with open(args.params) as f_params:
+        params = json.load(f_params)
+
+    job_params = params[job_id]
+    run_command = create_run_command(job_params)
+    subprocess.run(run_command)
