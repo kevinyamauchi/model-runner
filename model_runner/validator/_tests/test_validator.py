bd1cd16e289de47bd208313a960ffb429a447b7d
dsethz <42010290+dsethz@users.noreply.github.com>
2021-11-03 10:16:08 +0100

    add validator (#6)
    * add validator
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Yamauchi <kevin.yamauchi@gmail.com>
    
    * apply changes from code review
    
    * assert windows path seperator compatibility
    
    Co-authored-by: Kevin Yamauchi <kevin.yamauchi@gmail.com>

---
+++ model_runner/validator/_tests/test_validator.py
@@ -0,0 +1,92 @@
+import os
+
+import pytest
+from pydantic import ValidationError
+
+from model_runner.validator.validator_utils import ConfigModel
+
+
+@pytest.fixture(scope="session")
+def base_config(tmp_path_factory):
+    my_file = tmp_path_factory.mktemp("data") / "myfile"
+    my_file.touch()
+
+    config = {
+        "job_prefix": "my_experiment",
+        "runner_parameters": {
+            "data": [tmp_path_factory._basetemp.as_posix()],
+            "augment": [True, False],
+            "batch_size": [2, 16, 32],
+            "lr": [5e-05, 0.001],
+        },
+        "job_parameters": {
+            "gpu_type": "TITANRTX",
+            "logfile_dir": tmp_path_factory._basetemp.as_posix(),
+            "memory": 4000,
+            "ngpus": 2,
+            "njobs_parallel": 4,
+            "processor_cores": 16,
+            "run_time": "3:00",
+            "scratch": 4000,
+        },
+        "runner": my_file.as_posix(),
+        "output_base_dir": tmp_path_factory._basetemp.as_posix(),
+    }
+
+    return config
+
+
+def test_good_config(base_config):
+    config = ConfigModel(**base_config)
+
+    # test some specific parameters
+    assert (
+        type(config.dict()["job_prefix"]) == str
+    ), '"job_prefix" should be of type str.'
+    assert (
+        type(config.dict()["job_parameters"]["run_time"]) == int
+    ), '"run_time" should be of type int.'
+    assert config.dict()["job_parameters"]["logfile_dir"].endswith(
+        os.path.sep
+    ), f'"logfile_dir" does not end with "{os.path.sep}".'
+    assert os.path.isdir(
+        config.dict()["output_base_dir"]
+    ), '"output_base_dir" is not a directory'
+
+
+def test_bad_configs(base_config):
+    # test gpu_type
+    bad_config = base_config.copy()
+    bad_config["job_parameters"].pop("gpu_type")
+    bad_config["job_parameters"]["gpu_type"] = "Titanium"
+
+    with pytest.raises(ValidationError):
+        _ = ConfigModel(**bad_config)
+
+    # test run_time
+    bad_config = base_config.copy()
+    bad_config["job_parameters"].pop("run_time")
+    bad_config["job_parameters"]["run_time"] = "3:120"
+
+    with pytest.raises(ValidationError):
+        _ = ConfigModel(**bad_config)
+
+    # test data
+    bad_config = base_config.copy()
+    bad_config["runner_parameters"].pop("data")
+
+    with pytest.raises(ValidationError):
+        _ = ConfigModel(**bad_config)
+
+    bad_config["runner_parameters"]["data"] = ["/some/bad/path"]
+
+    with pytest.raises(ValidationError):
+        _ = ConfigModel(**bad_config)
+
+    # test runner
+    bad_config = base_config.copy()
+    bad_config.pop("runner")
+    bad_config["runner"] = "/non/existant/file"
+
+    with pytest.raises(ValidationError):
+        _ = ConfigModel(**bad_config)
---
+++ model_runner/validator/validator_utils.py
@@ -0,0 +1,179 @@
+import os
+from typing import Any, Dict, List, Union
+
+from pydantic import BaseModel, validator
+
+
+class JobArrayModel(BaseModel):
+    """
+    pydantic BaseModel that handles the job_parameters.
+
+    Parameters
+    ----------
+    run_time: Union[str, int]
+        The time resources on the compute note are reservered for a single job. Corresponds to the bsub "-W"-flag.
+    processor_cores: int
+        Number of processor cores requested for a single job. Corresponds to the bsub "-n"-flag.
+    memory: int
+        Amount of memory requested per processor core. Corresponds to bsub -R "rusage[mem={memory}]".
+    scratch: int
+        Amount of local scratch requested per processor core. Corresponds to bsub -R "rusage[scratch={scratch}]".
+    gpu_type: str
+        Type of gpu that is accepted by bsub -R "select[gpu_model0=={gpu_type}]".
+    ngpus: int
+        Number of GPUs of {gpu_type} requested. Corresponds to bsub -R "rusage[ngpus_excl_p={ngpus}]".
+    njobs_parallel: int
+        Number of jobs the job array will submit in parallel.
+    logfile_dir: str
+        Path to directory to which lsf output files are saved.
+    """
+
+    run_time: Union[str, int]
+    processor_cores: int
+    memory: int
+    scratch: int
+    gpu_type: str
+    ngpus: int
+    njobs_parallel: int
+    logfile_dir: str
+
+    @validator("logfile_dir")
+    def logfile_dir_is_dir(cls, v):
+        """
+        Validate if logfile_dir is an existing directory.
+        """
+        if not os.path.isdir(v):
+            raise ValueError(f'"{v}" is not a directory.')
+        elif not v.endswith(os.path.sep):
+            v += os.path.sep
+
+        return v
+
+    @validator("gpu_type")
+    def gpu_type_exists(cls, v):
+        """
+        Validate if submitted gpu_type is currently available at Euler
+        (https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#GPU).
+        """
+        gpu_list = [
+            "GeForceGTX1080",
+            "GeForceGTX1080Ti",
+            "GeForceRTX2080Ti",
+            "GeForceRTX2080Ti",
+            "TITANRTX",
+            "TeslaV100_SXM2_32GB",
+            "TeslaV100_SXM2_32GB",
+            "A100_PCIE_40GB",
+        ]
+
+        if v not in gpu_list:
+            raise ValueError(
+                f'"{v}" is currently not available on Euler. Check the list of available GPUs '
+                "at https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#GPU."
+            )
+
+        return v
+
+    @validator("run_time")
+    def validate_and_coerce_run_time(cls, v):
+        """
+        Validate if submitted run_time adheres to the format specified in
+        https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#Wall-clock_time.
+        """
+        if type(v) == str:
+            v_split = v.split(":")
+            if len(v_split) == 1:
+                try:
+                    minutes = int(v_split[0])
+                    v = minutes
+                except ValueError:
+                    raise ValueError(
+                        "run_time string must adhere to the format specified in "
+                        "https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#Wall-clock_time."
+                    )
+            elif len(v_split) == 2:
+                try:
+                    minutes = int(v_split[1])
+                    if (minutes > 59) or (minutes < 0):
+                        raise ValueError(
+                            "run_time string must adhere to the format specified in "
+                            "https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#Wall-clock_time."
+                        )
+
+                    hours = int(v_split[0])
+                    v = minutes + hours * 60
+                except ValueError:
+                    raise ValueError(
+                        "run_time string must adhere to the format specified in "
+                        "https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#Wall-clock_time."
+                    )
+            else:
+                raise ValueError(
+                    "run_time string must adhere to the format specified in "
+                    "https://scicomp.ethz.ch/wiki/Getting_started_with_clusters#Wall-clock_time."
+                )
+
+        return v
+
+
+class ConfigModel(BaseModel):
+    """
+    pydantic BaseModel that handles the job_config.json.
+
+    Parameters
+    ----------
+    runner: str
+        Path to the runner file.
+    job_prefix: str
+        Prefix that precedes all results folders and experiment specific files.
+    output_base_dir: str
+        Directory to which all config files and results folders are saved.
+    job_parameters: Dict[str, Any]
+        Dictionary of parameters for the job array command that are handled by the JobArrayModel.
+    runner_parameters: Dict[str, List[Any]]
+        Dictionary containing the parameters grid submitted to the runner.
+    """
+
+    job_prefix: str
+    runner: str
+    output_base_dir: str
+    job_parameters: JobArrayModel
+    runner_parameters: Dict[str, List[Any]]
+
+    @validator("output_base_dir")
+    def output_base_dir_is_dir(cls, v):
+        """
+        Validate if output_base_dir is an existing directory.
+        """
+        if not os.path.isdir(v):
+            raise ValueError(f'"{v}" is not a directory.')
+        elif not v.endswith(os.path.sep):
+            v += os.path.sep
+
+        return v
+
+    @validator("runner_parameters")
+    def data_in_runner_parameters_and_file_or_path(cls, v):
+        """
+        Validate if "data" key is in runner_parameters and if it is either file or directory.
+        """
+        # check if runner_parameters contains data key
+        assert "data" in v.keys()
+
+        # check if all paths in "data" are existing files or directories
+        for i, f in enumerate(v["data"]):
+            if not (os.path.isfile(f) or os.path.isdir(f)):
+                raise ValueError(f'"{f}" is not a file/directory.')
+            else:
+                if os.path.isdir(f) and not f.endswith(os.path.sep):
+                    v["data"][i] = f + os.path.sep
+        return v
+
+    @validator("runner")
+    def runner_is_file(cls, v):
+        """
+        Validate if "runner" is an existing file.
+        """
+        if not os.path.isfile(v):
+            raise ValueError('"runner" is not a file.')
+        return v
